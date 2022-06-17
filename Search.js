073602c71154c8f98f76e73a9cf45c93df21b8d1
Milan Jobanputra <107670121+Milan-Jobanputra@users.noreply.github.com>
2022-06-17 12:06:33 +0530

    Create Search.js

---
+++ Search.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    // Setup - add a text input to each footer cell
+    $('#tabdata tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    });
+ 
+    // DataTable
+    var table = $('#tabdata').DataTable({
+        initComplete: function () {
+            // Apply the search
+            this.api()
+                .columns()
+                .every(function () {
+                    var that = this;
+ 
+                    $('input', this.footer()).on('keyup change clear', function () {
+                        if (that.search() !== this.value) {
+                            that.search(this.value).draw();
+                        }
+                    });
+                });
+        },
+    });
+});
